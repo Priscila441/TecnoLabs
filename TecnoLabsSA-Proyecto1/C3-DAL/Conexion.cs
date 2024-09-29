21d4272c7575b333e25107b5eafdfebc9cd0f166
Rocio <rociomedran9@gmail.com>
2024-09-29 18:03:40 -0300

    Metodos para mostrar productos

--- TecnoLabsSA-Proyecto1/C3-DAL/Conexion.cs
+++ TecnoLabsSA-Proyecto1/C3-DAL/Conexion.cs
@@ -5,45 +5,80 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using C4_ENTIDADES;
-using System.Runtime.Remoting.Messaging;
-
 
 namespace C3_DAL
 {
-    internal class Conexion
+    public class Conexion
     {
-        //creamos obj de sus clases para usar metodos
-        SqlConnection conexion = new SqlConnection();
-        SqlCommand comando = new SqlCommand();
+        SqlConnection conexion;
+        SqlCommand comando;
+        SqlDataReader lector;
 
-        public List<Persona> LeerPersonas()
+        private string connectionString = "Data Source =PHOENIX\\MSSQLSERVER01; Intitial Catalog =PracticoGrupal; Integrated Security =sspi";
+
+        public Conexion()
         {
-             List<Persona> listPersona = new List<Persona>();
-            conexion.ConnectionString = "data source = DESKTOP-UN9UCN3\\GABI_SQL; initial catalog =TecnoLabs; integrated security = sspi";
-            comando.CommandType = System.Data.CommandType.Text; 
+            conexion = new SqlConnection(connectionString);
+            comando = new SqlCommand();
             comando.Connection = conexion;
-            conexion.Open();
-
-            SqlDataReader lector = comando.ExecuteReader();
-            while (lector.Read())
-            {
-                Persona p = new Persona();
-
-
-            }
-
-
-
-
-
-            return listPersona;
+            comando.CommandType = System.Data.CommandType.Text;
         }
 
-       
+        public List<ProductoDTO> LeerProductos()
+        {
+            List<ProductoDTO> listProductos = new List<ProductoDTO>();
 
-       
-          
-          
+            conexion.Open();
+            comando.CommandText = "SELECT * FROM VistaProductos";
 
+            lector = comando.ExecuteReader();
+            while (lector.Read())
+            {
+                ProductoDTO producto = new ProductoDTO();
+                producto.Categoria = lector.GetString(0);
+                producto.Marca = lector.GetString(1);
+                producto.Modelo = lector.GetString(2);
+                producto.Precio = lector.GetDecimal(3);
+
+                listProductos.Add(producto);
+            }
+
+            conexion.Close();
+            return listProductos;
+        }
+        public List<ProductoDTO> FiltrarPorMarca(string marca, string orden)
+        {
+            List<ProductoDTO> listProductos = new List<ProductoDTO>();
+
+            conexion.Open();
+
+            comando.Parameters.Clear();
+            comando.Parameters.AddWithValue("@Marca", marca);
+            if (orden == "Menor")
+            {
+                comando.CommandText = "SELECT * FROM ViewProductos WHERE Marca = @Marca ORDER BY Precio ASC";
+            }
+            else if (orden == "Mayor")
+            {
+                comando.CommandText = "SELECT * FROM ViewProductos WHERE Marca = @Marca ORDER BY Precio DESC";
+            }
+            else
+            {
+                comando.CommandText = "SELECT * FROM ViewProductos WHERE Marca = @Marca";
+            }
+            lector = comando.ExecuteReader();
+            while (lector.Read())
+            {
+                ProductoDTO producto = new ProductoDTO();
+                producto.Categoria = lector.GetString(0);
+                producto.Marca = lector.GetString(1);
+                producto.Modelo = lector.GetString(2);
+                producto.Precio = lector.GetDecimal(3);
+
+                listProductos.Add(producto);
+            }
+            conexion.Close();
+            return listProductos;
+        }
     }
 }
